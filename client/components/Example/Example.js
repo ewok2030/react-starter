33549230e24095b8f14d29d221d07fe1259c1f5c
Michael Palumbo <mpalumbo7@gmail.com>
2017-03-03 00:44:56 -0500

    Added eslint config and fixed some minor bugs

--- client/components/Example/Example.js
+++ client/components/Example/Example.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default class Example extends React.Component {
+  static propTypes = {
+    title: React.PropTypes.string.isRequired,
+  }
+
   render() {
     return (
       <div>
         <div className="container">
-          <p className="text-muted">Place some HTML here.</p>
+          <p className="text-muted">{this.props.title}</p>
         </div>
       </div>
     );
